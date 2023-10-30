746a81e267004671c9a9ac36825fd42adf0be47d
Elizaveta <aliza-97@mail.ru>
2023-10-30 19:09:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int prise = 54678; // рублей
+        int x = 20;
+        int miles = prise/x;
+
+        System.out.println(miles);
+
+    }
+}
