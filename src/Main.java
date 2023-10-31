f3d896d474047a18accf3e709a2d5222e92b51d5
Elizaveta <aliza-97@mail.ru>
2023-10-31 22:56:06 +0300

    change mistake

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         int prise = 54678; // рублей
-        int x = 20;
-        int miles = prise/x;
+        int bonus = 20;
+        int miles = prise / bonus;
 
         System.out.println(miles);
 
